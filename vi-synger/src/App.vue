74f5152334e0d756128f32228a0f75c1acbc2de8
emilschultz <emil.schultz@amedia.no>
2023-02-01 22:08:49 +0100

    begin migration

--- vi-synger/src/App.vue
+++ vi-synger/src/App.vue
@@ -1,91 +1,63 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+// import { RouterLink, RouterView } from "vue-router";
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export interface SongData {
+  song: string;
+}
+const requestSongs = new Request("https://kjoller.p.rapidapi.com/songs");
+const songSection = document.getElementById("allSongs");
+
+window.addEventListener("load", () => {
+  getSongs();
+});
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "00c14e5c06mshe839a2f8fc92b63p133489jsnd629f0a2537c",
+    "X-RapidAPI-Host": "kjoller.p.rapidapi.com",
+  },
+};
+
+async function getSongs() {
+  const res = await fetch(requestSongs, options);
+  const json = await res.json();
+  console.log(json.data);
+  let tagOne;
+  let tagTwo;
+  let tagThree;
+  // songData.push(data);
+  json.data.forEach((song) => {
+    tagOne = song.tags[0];
+    tagTwo = song.tags[1];
+    tagThree = song.tags[2];
+
+    // songSection.innerHTML += `<song-card title="${song.title}" text="${
+    //   song.text
+    // }" melody="${song.melody}" tag1="${tagOne}" tag2="${tagTwo || ""}" tag3="${
+    //   tagThree || ""
+    // }"></song-card>`;
+  });
+}
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <section class="songSection">
+    <div>{{ song }}</div>
+  </section>
+  <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style scoped></style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<!-- <header>
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+    
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div> 
+</header> -->
